1ffa885189f2eb8be09073dd3428f4244b662720
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-08-06 21:33:40 +0530

    first part complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do 
+    resources :messages
+  end
   root 'home#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  get "user/:id", to: "users#show", as: "user"
 end
